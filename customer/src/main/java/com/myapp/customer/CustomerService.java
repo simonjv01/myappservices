e0d967ef8388c1863ee4ba26c2133ee797987e4c
Simon Vargas <sjvargas01@gmail.com>
2022-09-27 22:08:21 -0400

    first commit

---
+++ customer/src/main/java/com/myapp/customer/CustomerService.java
@@ -0,0 +1,19 @@
+package com.myapp.customer;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService() {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        // todo: check if email is valid
+        // todo: check if email is not taken
+        // todo: store customer in db
+    }
+}
